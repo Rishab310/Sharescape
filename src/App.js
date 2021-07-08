003680865335e40c484cf1b2623c8abbf1610774
Rishab Goyal <rishabgoyal432@gmail.com>
2021-07-09 02:45:50 +0530

    Sign in Sign Up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layouts/Navbar';
 import PostDetail from './components/post/PostDetail';
@@ -11,6 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard}/>
           <Route exact path="/post/:id" component={PostDetail}/>
+          <Route exact path="/signin" component={SignIn}/>
+          <Route exact path="/signup" component={SignUp}/>
         </Switch>
       </div>
     </BrowserRouter>
