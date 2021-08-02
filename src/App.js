b01ce86787f7a5ef7a870caeab4ffd0344fa7b83
Rishab Goyal <rishabgoyal432@gmail.com>
2021-08-03 02:58:01 +0530

    Splash Screen

--- src/App.js
+++ src/App.js
@@ -6,19 +6,30 @@ import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layouts/Navbar';
 import CreatePost from './components/post/CreatePost';
 import PostDetail from './components/post/PostDetail';
+import { isLoaded } from 'react-redux-firebase'
+import { useSelector } from 'react-redux';
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div></div>;
+  return children
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Dashboard}/>
-          <Route exact path="/post/:id" component={PostDetail}/>
-          <Route exact path="/signin" component={SignIn}/>
-          <Route exact path="/signup" component={SignUp}/>
-          <Route exact path="/create" component={CreatePost}/>
-        </Switch>
-      </div>
+      <AuthIsLoaded>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/post/:id" component={PostDetail} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/create" component={CreatePost} />
+          </Switch>
+        </div>
+      </AuthIsLoaded>
     </BrowserRouter>
   );
 }
