cb653c8700024ba438e5be6a81feb2d35a6ce40b
Rishab Goyal <rishabgoyal432@gmail.com>
2021-08-03 01:43:30 +0530

    Wait for auth isLOaded

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -3,18 +3,22 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">WeShare</Link>
-        <SignedInLinks />
-        <SignedOutLinks/>
+        {auth.isLoaded && links}
       </div>
     </nav>
   );
 }
 const mapStateToProps = (state) => {
-  return {}
+  return {
+    auth: state.firebase.auth
+  }
 }
 export default connect(mapStateToProps)(Navbar);
